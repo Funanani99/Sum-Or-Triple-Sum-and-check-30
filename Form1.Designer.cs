e1fb807507e74dd60d83546dda027926d10006db
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-16 18:31:19 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Sum_Or_Triple_Sum_and_check_30
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResultCheck = new System.Windows.Forms.TextBox();
+            this.txResultSum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sum Or Triple Sum";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(567, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Check 30";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter first number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter second number:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(308, 100);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(151, 26);
+            this.txtNum1.TabIndex = 4;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(308, 146);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(151, 26);
+            this.txtNum2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sum:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(521, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Results:";
+            // 
+            // txtResultCheck
+            // 
+            this.txtResultCheck.BackColor = System.Drawing.SystemColors.Info;
+            this.txtResultCheck.Location = new System.Drawing.Point(606, 297);
+            this.txtResultCheck.Name = "txtResultCheck";
+            this.txtResultCheck.Size = new System.Drawing.Size(99, 26);
+            this.txtResultCheck.TabIndex = 8;
+            // 
+            // txResultSum
+            // 
+            this.txResultSum.BackColor = System.Drawing.SystemColors.Info;
+            this.txResultSum.Location = new System.Drawing.Point(210, 300);
+            this.txResultSum.Name = "txResultSum";
+            this.txResultSum.Size = new System.Drawing.Size(90, 26);
+            this.txResultSum.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txResultSum);
+            this.Controls.Add(this.txtResultCheck);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Sum Or Triple Sum And Check 30";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResultCheck;
+        private System.Windows.Forms.TextBox txResultSum;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sum_Or_Triple_Sum_and_check_30
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            int result = SumOrTripleSum(num1, num2);
+            txResultSum.Text =  result.ToString();
+        }
+
+        private int SumOrTripleSum(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                return 3 * (num1 + num2);
+            }
+            else
+            {
+                return num1 + num2;
+            }
+        }
+
+         private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            bool result = Check30(num1, num2);
+            txtResultCheck.Text = result.ToString();
+        }
+
+        private bool Check30(int num1, int num2)
+        {
+            return (num1 == 30 || num2 == 30 || (num1 + num2) == 30);
+        }
+    }
+
+}
+
